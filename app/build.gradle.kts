539c04ef547b5ed9760db1f4b9955217528d74a6
ella-shin <Ella.Shin@obigo.com>
2025-02-25 13:44:05 +0900

    Update build.gradle(v2.3 -> v2.4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tsdm"
         minSdk = 28
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.3"
+        versionCode = 3
+        versionName = "2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
